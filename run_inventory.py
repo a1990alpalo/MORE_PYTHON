55cf90a4606ba9717978403223dbfc3f3be38605
Alberto Medina <a1990alpalo@hotmail.com>
2026-06-18 23:30:37 -0400

    adding the run inventory.py file

---
+++ run_inventory.py
@@ -0,0 +1,7 @@
+from inventory import products, display_inventory, display_total_value
+
+print("This file is running code imported from inventory.py\n")
+
+display_inventory(products)
+display_total_value(products)
+
